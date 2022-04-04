c41164e0caaf9b34f6c0d9584286f1b96231d89f
osewe <s.osewemwai@gmail.com>
2022-04-04 06:58:34 +0300

    Refactoring

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,4 @@ if(typeof userInput === 'string'){
    console.log(userName);
 }
 
-function generateError(message: string, code: number): never{
-   throw {message: message, errorCode: code};
-}
 
-generateError('An error occured', 400);
